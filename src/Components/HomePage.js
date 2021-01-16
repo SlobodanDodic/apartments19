37be242000670c8b7b4ea46414773cec9eccae68
SlobodanDodic <slobodandodic@gmail.com>
2021-01-16 22:20:57 +0100

    New home page

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -1,22 +1,22 @@
 import React from "react";
 import Grid from "@material-ui/core/Grid";
 import Container from "@material-ui/core/Container";
-import logo from "../Assets/logo.png";
+//import logo from "../Assets/icons/logo.png";
+import gif from "../Assets/icons/stars.gif";
 
 const HomePage = () => {
   return (
     <Grid container className="homepage" id="home">
       <Container>
-        <div className="div-text">
-          <h2 className="home-text">Apartments</h2>
-          <h2 className="home-text">Sombor 19</h2>
+        <div className="home-text">
+          Apartments <br /> Sombor 19
         </div>
 
-        <img className="logo" src={logo} alt="logo" />
+        {/* <img className="logo" src={logo} alt="logo" /> */}
+        <img className="gif" src={gif} alt="gif" />
 
         <div className="div-text-phone">
-          <h2>BOOK NOW</h2>
-          <h3>+381 63 540 433</h3>
+          <p>Book now: +381 63 540 433</p>
         </div>
       </Container>
     </Grid>
